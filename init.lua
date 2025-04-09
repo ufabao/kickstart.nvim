22a0ad9d0caa74657ea56da56cc3a8a77b34a68c
micah <micahdarrell1@gmail.com>
2025-04-08 20:34:21 -0400

    blah

--- init.lua
+++ init.lua
@@ -44,7 +44,7 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -103,6 +103,12 @@ vim.opt.scrolloff = 10
 -- See `:help 'confirm'`
 vim.opt.confirm = true
 
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+vim.opt.expandtab = true
+vim.opt.autoindent = true
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
